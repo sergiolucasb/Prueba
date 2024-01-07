5260182daff62483fefef8d0d5855d343fd6fd5b
Sergio Lucas Ballester <sergiolucasballester@gmail.com>
2024-01-07 18:38:44 +0100

    Clase asistente creada - atributos

--- src/Asistente.java
+++ src/Asistente.java
@@ -1,2 +1,10 @@
 public class Asistente {
+    //ATRIBUTOS
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String telefono;
+    private String dni;
+    private String fecha_nacimiento;
+    
 }
