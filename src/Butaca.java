2109ecc24a5e3e89a30ca4d5e3cbab49b3c6ad2f
Sergio Lucas Ballester <sergiolucasballester@gmail.com>
2024-01-07 18:55:01 +0100

    Clase Butaca creada - constructores

--- src/Butaca.java
+++ src/Butaca.java
@@ -4,5 +4,11 @@ public class Butaca {
     private boolean accesibilidad;
     private boolean disponible;
 
-    
+    //CONSTRUCTORES
+    Butaca(){}
+    Butaca(String id, boolean accesibilidad, boolean disponible) {
+        this.id = id;
+        this.accesibilidad = accesibilidad;
+        this.disponible = disponible;
+    }
 }
