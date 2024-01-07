9278862214fd5588db0de3f54dc048a6fecf58bf
Sergio Lucas Ballester <sergiolucasballester@gmail.com>
2024-01-07 19:06:09 +0100

    Clase Sala creada - atributos

--- src/Sala.java
+++ src/Sala.java
@@ -1,2 +1,11 @@
 public class Sala {
+    //ATRIBUTOS
+    private String nombre;
+    private int capacidad_maxima;
+    private String lista_butacas;
+    private double dimension;
+    private boolean accesibilidad;
+    private String caracteristicas_acusticas;
+    private String observaciones;
+
 }
