6690b171fe0169c3d6895cf57d511ac1cc6b9a75
Sergio Lucas Ballester <sergiolucasballester@gmail.com>
2024-01-07 19:20:28 +0100

    Clase Reserva creada - atributos

--- src/Reserva.java
+++ src/Reserva.java
@@ -1,2 +1,10 @@
 public class Reserva {
+    //ATRIBUTOS
+    private String id;
+    private Asistente asistente;
+    private Evento evento;
+    private Butaca butaca;
+    private String fecha;
+    private String hora;
+    
 }
