1ee5c63094a3a9da967c3cc8e7699b393952a30f
Sergio Lucas Ballester <sergiolucasballester@gmail.com>
2024-01-07 18:43:11 +0100

    Clase asistente creada - constructores

--- src/Asistente.java
+++ src/Asistente.java
@@ -6,5 +6,22 @@ public class Asistente {
     private String telefono;
     private String dni;
     private String fecha_nacimiento;
+
+    //CONSTRUCTORES
+    Asistente(){}
+    Asistente(String nombre, String apellidos, String email, String telefono, String dni, String fecha_nacimiento){
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.telefono = telefono;
+        this.dni = dni;
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+    Asistente(String nombre, String apellidos, String email) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+    }
+
     
 }
